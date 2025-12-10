d6494e9ffb0c878e1fca08f2f71797e1db36b26f
cahangeorge <cahangeorge@gmail.com>
2025-12-10 17:16:41 +0200

    feat(test): add Jest configuration for unit tests

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.module.ts',
+    '!**/index.ts',
+    '!**/*.interface.ts',
+    '!**/*.dto.ts',
+    '!**/main.ts',
+    '!**/seeds/**',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(uuid)/)',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/../test/setup.ts'],
+  testTimeout: 30000,
+};
